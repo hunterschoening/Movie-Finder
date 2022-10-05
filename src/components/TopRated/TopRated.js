3e43556e763c52b20458eb3261278ce177e4559d
Hunter Schoening <hunter.schoening@gmail.com>
2022-10-04 20:48:21 -0600

    Added infinite scroll to all components

--- src/components/TopRated/TopRated.js
+++ src/components/TopRated/TopRated.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Container, Grid } from '@mui/material';
+import { Container } from '@mui/material';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 import Movies from '../Movies/Movies';
 
 import { getTopRatedMovies } from '../../actions/movies';
 
+let page = 1;
+
 const TopRated = () => {
     const dispatch = useDispatch();
     const { movies } = useSelector((state) => state.movies);
 
+    const fetchData = () => {
+        dispatch(getTopRatedMovies(page));
+        page = page + 1;
+    }
+
     useEffect(() => {
         dispatch(getTopRatedMovies());
+        page = page + 1;
     }, [])
 
     return (
-        <Container maxWidth="xl" sx={{ paddingTop: "15px" }}>
-            <Movies />
-        </Container>
+        <InfiniteScroll
+            dataLength={movies.length}
+            next={() => {
+                fetchData()
+            }}
+            hasMore={true}
+            loader={<h4>Loading...</h4>}
+            endMessage={<h4>You've seen it all!</h4>}
+        >
+            <Container maxWidth="xl" sx={{ paddingTop: "15px" }}>
+                <Movies />
+            </Container>
+        </InfiniteScroll>
     )
 }
 
